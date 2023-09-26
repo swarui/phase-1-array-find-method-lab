4e795bf23f52b544e8c8782456029cd021d7ce5d
stevewarui,@gmail.com <stevewarum@gmail.com>
2023-09-26 23:00:51 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+const record = [
+  { year: "2015", result: "W" },
+  { year: "2013", result: "L" },
+];
+
+function superbowlWin(record) {
+  for (const item of record) {
+    if (item.result === "W") {
+      return item.year
+    }
+  }
+}
+record.find(superbowlWin)
